bd1c2ff3c3d752f45e69eb83cae7ef6b48d5041f
tonkatsutei <info@tonkatsutei.com>
2023-01-02 16:09:31 +0900

    1.2.0 [Ver.1.2.0] 2023/01/02 「未使用サイズはImage Size Managerの管理データから削除する」スイッチを付けた 標準プラグインもImage Size Managerで管理するようにした     それに伴い「設定 > メディア」メニューを非表示に     Image Size ManagerのDBも併用する案は却下 RESETを実装 プログラムの全体的な見直し

--- main.php
+++ main.php
@@ -4,12 +4,21 @@ Plugin Name: Image Size Manager
 Plugin URI: https://manual.tonkatsutei.com/image_size_manager/
 Description: 利用しないサムネイルファイルの生成を停止します。
 Author: ton活亭
-Version: 1.1.0
+Version: 1.2.0
 Author URI: https://twitter.com/tonkatsutei
 
-バージョン履歴
+
+### バージョン履歴 ###
+[Ver.1.2.0] 2023/01/02
+「未使用サイズはImage Size Managerの管理データから削除する」スイッチを付けた
+標準プラグインもImage Size Managerで管理するようにした
+    それに伴い「設定 > メディア」メニューを非表示に
+    Image Size ManagerのDBも併用する案は却下
+RESETを実装
+プログラムの全体的な見直し
+
 [Ver.1.1.0] 2022/12/27
-今後増えるサイズもすべて自動的にOFFにするスイッチを付けた
+「今後増えるサイズもすべて自動的にOFFにする」スイッチを付けた
 [Ver.1.0.1] 2022/12/26
 ・自動バージョンアップのテスト
 [Ver.1.0.0] 2022/12/26
@@ -37,5 +46,5 @@ $myUpdateChecker = PucFactory::buildUpdateChecker(
 );
 $myUpdateChecker->setBranch('master');
 
-// 本体
+// 本体読込
 require_once('include/base.php');
